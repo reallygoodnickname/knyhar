18006003470652ee8ede2cd052904bf3f2983c36
reallygoodnickname <78232370+reallygoodnickname@users.noreply.github.com>
2024-02-25 18:26:24 +0200

    feat: implement books endpoint

--- knyhar/api/books.py
+++ knyhar/api/books.py
@@ -1,33 +1,70 @@
 # Books endpoint
 
-from fastapi import APIRouter
+from knyhar.models.books import Book, BookModel
+from fastapi import APIRouter, Response
+from fastapi.requests import Request
 from pydantic import BaseModel
 
 books_endpoint = APIRouter(prefix="/books",
                            tags=["books"])
 
 
-class Book(BaseModel):
-    name: str
-    description: str
-    author: list[str]
-    genre: list[str]
-    price: float
-
-
-# Get all books or info about specific book
 @books_endpoint.get("/")
-def get_books(id: int | None = None):
-    return {"msg: not implemented!"}, 501
+def get_all_books(request: Request, response: Response):
+    # Return all books in JSON format
+    books = request.app.extra["database"].books
+
+    response.status_code = 200
+
+    _books = books.get_all()
+    res = []
+
+    for book in _books:
+        res.append(book.get_pydantic_model())
+
+    return res
 
 
-# Add new book (admin only)
+@books_endpoint.get("/{id}")
+def get_book(request: Request, response: Response, id: int):
+    # Get info about one specific book
+    books = request.app.extra["database"].books
+
+    book = books.get(id)
+    if book is None:
+        response.status_code = 400
+        return {"code": 400,
+                "msg": "Book with such ID doesn't exist!"}
+
+    return book.get_pydantic_model()
+
+
 @books_endpoint.post("/")
-def add_book(book: Book):
-    return {"msg: not implemented!"}, 501
+def add_book(request: Request, book: BookModel, response: Response):
+    books = request.app.extra["database"].books
+
+    _book = Book(name=book.name, description=book.description,
+                 author=book.author, price=book.price)
+
+    if not books.add(_book):
+        response.status_code = 400
+        return {"code": 400,
+                "msg": "Book already exists!"}
+    else:
+        response.status_code = 200
+        return {"code": 200,
+                "msg": "Added successfully!"}
 
 
-# Delete book by id (admin only)
-@books_endpoint.post("/")
-def delete_book(id: int):
-    return {"msg: not implemented"}, 501
+@books_endpoint.delete("/{id}")
+def delete_book(request: Request, id: int, response: Response):
+    books = request.app.extra["database"].books
+
+    if not books.remove(id):
+        response.status_code = 400
+        return {"code": 400,
+                "msg": "Book doesn't exist!"}
+    else:
+        response.status_code = 200
+        return {"code": 200,
+                "msg": "Book removed successfully!"}
