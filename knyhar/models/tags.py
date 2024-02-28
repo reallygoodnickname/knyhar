cc5955a6c17c150163883a933c962c9147c42dc1
reallygoodnickname <78232370+reallygoodnickname@users.noreply.github.com>
2024-02-28 21:57:52 +0200

    chore: add pydantic tag model and function for getting it

--- knyhar/models/tags.py
+++ knyhar/models/tags.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from pydantic import BaseModel
+
 from knyhar.models import Base, books_tags_assoc_table
 from typing import List
 
@@ -9,14 +11,22 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 import knyhar.models.books
 
 
+class TagModel(BaseModel):
+    name: str
+
+
 class Tag(Base):
     __tablename__ = "tags"
 
     name: Mapped[str] = mapped_column(
         String(32), primary_key=True, nullable=False, unique=True)
+
     books: Mapped[List[knyhar.models.books.Book]] = relationship(
         secondary=books_tags_assoc_table, back_populates="tags"
     )
+
+    def get_pydantic_model(self) -> TagModel:
+        return TagModel(name=self.name)
 
     def __repr__(self):
         return f'Tag(name={self.name}, books={self.books})'
