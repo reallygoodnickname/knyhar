6f5f41a775cd5f5bb85574c4d647542e77aa713e
reallygoodnickname <78232370+reallygoodnickname@users.noreply.github.com>
2024-02-29 13:43:29 +0200

    chore: add function wrappers for adding favorite books

--- knyhar/database/users.py
+++ knyhar/database/users.py
@@ -1,4 +1,6 @@
 # Subclass used for functions related to users
+from sqlalchemy import select
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm import Session
 
 from knyhar.models.users import User
@@ -9,9 +11,29 @@ class UsersDatabase(SubclassTemplate):
     def __init__(self, database_obj):
         super().__init__(database_obj, User)
 
+    def get_user_by_username(self, username: str) -> User | None:
+        """
+        Get user password by username
+
+        Arguments:
+            username: Username to look for
+        Returns:
+            User | None: This function will either return found user,
+                         or None, if user is not found
+        """
+        with Session(self.engine) as session:
+            user = select(User).where(User.username == username)
+
+            res = session.execute(user).first()
+
+            if res is not None:
+                return res[0]
+
+            return None
+
     def set_role(self, id: int, role: bool) -> bool:
         """
-        Grant or invoke user permissions 
+        Grant or invoke user permissions
 
         Arguments:
             id: Target user id
@@ -19,7 +41,7 @@ class UsersDatabase(SubclassTemplate):
                   True  - admin
                   False - regular
         Returns:
-            bool: True  - updated user's role 
+            bool: True  - updated user's role
                   False - user doesn't exist
         """
         with Session(self.engine) as session:
@@ -33,3 +55,28 @@ class UsersDatabase(SubclassTemplate):
             session.commit()
 
             return True
+
+    def add_favorite(self, user_id: int, book_id: int) -> None:
+        """
+        Add favorite book by id
+
+        Arguments:
+            user_id: User ID
+            book_id: Book ID
+        Returns:
+            None: This function doesn't return anything
+        """
+        return self.add_entity(user_id, book_id, "books", "favorites")
+
+    def remove_favorite(self, user_id: int, book_id: int) -> None:
+        """
+        Remove favorite book by id
+
+        Arguments:
+            user_id : User ID
+            book_id: Book ID
+        Returns:
+            None: This function doesn't return anything
+        """
+
+        return self.remove_entity(user_id, book_id, "books", "favorites")
