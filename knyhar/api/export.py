4907d7f4a94b3ab2d9946614bbd39002290eeea2
reallygoodnickname <78232370+reallygoodnickname@users.noreply.github.com>
2024-02-29 10:50:26 +0200

    refactor: use pydantic models for exporting in csv

--- knyhar/api/export.py
+++ knyhar/api/export.py
@@ -4,38 +4,30 @@ from csv import writer
 
 from fastapi import APIRouter, Request
 from fastapi.responses import StreamingResponse
+from sqlalchemy.orm import Session
 
-from knyhar.models.books import Book
+from knyhar.models.books import BookModel
 
 endpoint = APIRouter(prefix="/export", tags=["export"])
 
 
-# Exports all books in CSV format (admin only)
 @endpoint.get("/")
 def export_books(request: Request):
     # Get all books from database
-    db = request.app.extra["database"]
-    books = db.books.get_all()
+    database = request.app.extra["database"]
+    books = database.books.get_all()
 
     out = StringIO()
     csv_writer = writer(out)
 
-    # Get all keys from that books table
-    mapper = Book.__mapper__
-    columns = mapper.columns.keys()
-    relationships = mapper.relationships.keys()
-
-    csv_writer.writerow(columns+relationships)
-
     # Convert object to csv
-    for book in books:
-        row = [getattr(book, column) for column in columns]
+    with Session(database.engine) as session:
+        dict_books = [book.get_pydantic_model(
+            session).model_dump() for book in books]
 
-        # Append tags (as names) and fans (as IDs)
-        row.append([tag.name for tag in book.tags])
-        row.append([fan.id for fan in book.fans])
-
-        csv_writer.writerow(row)
+        csv_writer.writerow(BookModel.__annotations__)
+        for book in dict_books:
+            csv_writer.writerow(book.values())
 
     # Make response
     response = StreamingResponse(iter([out.getvalue()]), media_type="text/csv")
